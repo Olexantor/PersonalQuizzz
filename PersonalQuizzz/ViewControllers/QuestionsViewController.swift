d8d8ceb54de34d0f85958eb8ade03b4b63cc57ee
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-02-11 02:02:45 +0500

    Work with logic of ResultViewController

--- PersonalQuizzz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuizzz/ViewControllers/QuestionsViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 
+    // MARK: IB Outlets
     @IBOutlet var questionProgressView: UIProgressView!
     @IBOutlet var questionLabel: UILabel!
-    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedSlider: UISlider! {
+        didSet {
+            let answerCount = Float(currentAnswers.count - 1)
+            rangedSlider.maximumValue = answerCount
+            rangedSlider.value = rangedSlider.maximumValue / 2
+        }
+    }
     
     @IBOutlet var singleStackView: UIStackView!
     @IBOutlet var multipleStackView: UIStackView!
@@ -22,20 +29,110 @@ class QuestionsViewController: UIViewController {
     @IBOutlet var multipleSwitches: [UISwitch]!
     @IBOutlet var rangedLabels: [UILabel]!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
+    //MARK: Private Properties
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var answersChoosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
     }
     
+    //MARK: Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultVC = segue.destination as! ResultsViewController
+        resultVC.answersChoosen = answersChoosen
+    }
+    
+    // MARK: IB Actions
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChoosen.append(currentAnswer)
+        nextQuestion()
     }
     
     @IBAction func multipleAnswerButtonPressed() {
+        for (multioleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multioleSwitch.isOn {
+                answersChoosen.append(answer)
+            }
+        }
+        
+        nextQuestion()
     }
     
     @IBAction func rangedAnswerButtonPressed() {
+        let index = lrintf(rangedSlider.value)
+        answersChoosen.append(currentAnswers[index])
+        nextQuestion()
+    }
+}
+
+//MARK: Private Methods
+extension QuestionsViewController {
+    private func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        let currentQuestion = questions[questionIndex]
+        questionLabel.text = currentQuestion.text
+        
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
     }
     
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: showMultipleStackView(with: currentAnswers)
+        case .ranged: showRangedStackView(with: currentAnswers)
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.text, for: .normal)
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden = false
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.text
+        }
+    }
+    
+    private func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden = false
+        
+        rangedLabels.first?.text = answers.first?.text
+        rangedLabels.last?.text = answers.last?.text
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
 }
+
--- PersonalQuizzz/ViewControllers/ResultsViewController.swift
+++ PersonalQuizzz/ViewControllers/ResultsViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    
+    //MARK: IB Outlets
+    @IBOutlet var resultEmojiLabel: UILabel!
+    @IBOutlet var resultDescriptionLabel: UILabel!
+    
+    //MARK: Public Properties
+    var answersChoosen: [Answer] = []
+ 
+    //MARK: Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        showResult()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Private Methods
+    private func showResult() {
+        
+        var dogCount = 0
+        var catCount = 0
+        var rabbitCount = 0
+        var turtleCount = 0
+        
+        for answerChoosen in answersChoosen {
+            switch answerChoosen.type {
+            case .dog:
+                dogCount += 1
+            case .cat:
+                catCount += 1
+            case .rabbit:
+                rabbitCount += 1
+            case .turtle:
+                turtleCount += 1
+            }
+        }
+        
+        let resultDictionary: [AnimalType: Int] = [
+            .dog: dogCount,
+            .cat: catCount,
+            .rabbit: rabbitCount,
+            .turtle: turtleCount
+        ]
+        
+        let result = resultDictionary.sorted { $0.value > $1.value }.first
+        if let result = result {
+            resultEmojiLabel.text = "ВЫ - \(result.key.rawValue)"
+            resultDescriptionLabel.text = result.key.definition
+        }
     }
-    */
-
 }
